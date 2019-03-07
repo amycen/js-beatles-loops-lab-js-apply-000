7996c5257e83f72e40a1db306347b0d076635936
Amy Cen <amy.cen@blackrock.com>
2019-03-07 20:46:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments){
   var i;
   var musiciansLength = musicians.length;
   var instrumentsLength = instruments.length;
-  
+
   for (i = 0; i < Math.min(musiciansLength, instrumentsLength); i++) {
     arr1[i] = musicians[i] + " plays " + instruments[i];
   }
